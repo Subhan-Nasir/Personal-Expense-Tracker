ca9b20ce211662824fb6e29a4fe6760149fc6d18
Subhan-Nasir <subhan.nasir561@gmail.com>
2022-11-26 20:55:49 +0000

    Commited app.py again

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from views import views
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "09c33a216982eff0f88ce875467a9ab4"
 app.register_blueprint(views, url_prefix="/")
-
+print("test commit")
 
 if __name__ == "__main__":
     app.run(debug=True)
